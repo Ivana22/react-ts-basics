75e26bb51b4215918ef3391593ce0452bb55af82
ivanatrpkova <ivana.trpkova@codeflair.ch>
2023-11-04 13:44:44 +0100

    add discriminated union for infobox mode

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,12 +1,20 @@
 import { type ReactNode } from "react";
 
-type InfoBoxProps = {
-  mode: "hint" | "warning";
-  severity?: "low" | "medium" | "high";
+type HintBoxProps = {
+  mode: "hint";
   children: ReactNode;
 };
 
-export default function InfoBox({ mode, children, severity }: InfoBoxProps) {
+type WarningBoxProps = {
+  mode: "hint" | "warning";
+  severity: "low" | "medium" | "high";
+  children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+  const { children, mode } = props;
   if (mode === "hint") {
     return (
       <aside className="infobox infobox-hint">
@@ -15,6 +23,7 @@ export default function InfoBox({ mode, children, severity }: InfoBoxProps) {
     );
   }
 
+  const { severity } = props;
   return (
     <aside className={`infobox infobox-warning warning--${severity}`}>
       <h2>Warning</h2>
